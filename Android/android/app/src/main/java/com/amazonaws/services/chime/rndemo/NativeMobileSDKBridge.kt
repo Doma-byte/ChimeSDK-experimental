7378fcc77b7d424534d6e84ce859a69cd11db104
Dikshit <singhdikshit69@gmail.com>
2024-01-24 15:23:26 +0530

    fix(NativeMobileSDKBridge.kt): import MediaDeviceType from the correct package to fix compilation error feat(NativeMobileSDKBridge.kt): add support for fetching audio devices list and switching between microphone and speaker feat(NativeMobileSDKBridge.kt): add support for switching camera

--- Android/android/app/src/main/java/com/amazonaws/services/chime/rndemo/NativeMobileSDKBridge.kt
+++ Android/android/app/src/main/java/com/amazonaws/services/chime/rndemo/NativeMobileSDKBridge.kt
@@ -9,7 +9,7 @@ import android.Manifest
 import android.content.pm.PackageManager
 import androidx.core.content.ContextCompat
 import com.amazonaws.services.chime.rndemo.RNEventEmitter.Companion.RN_EVENT_ERROR
-import com.amazonaws.services.chime.rndemo.RNEventEmitter.Companion.RN_EVENT_MEETING_END
+import com.amazonaws.services.chime.sdk.meetings.device.MediaDeviceType
 import com.amazonaws.services.chime.sdk.meetings.session.DefaultMeetingSession
 import com.amazonaws.services.chime.sdk.meetings.session.MeetingSession
 import com.amazonaws.services.chime.sdk.meetings.session.MeetingSessionConfiguration
@@ -18,17 +18,23 @@ import com.amazonaws.services.chime.sdk.meetings.session.MeetingSessionURLs
 import com.amazonaws.services.chime.sdk.meetings.session.defaultUrlRewriter
 import com.amazonaws.services.chime.sdk.meetings.utils.logger.ConsoleLogger
 import com.amazonaws.services.chime.sdk.meetings.utils.logger.LogLevel
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.ReadableMap
+import com.facebook.react.bridge.WritableArray
+import com.facebook.react.bridge.WritableMap
+import com.facebook.react.bridge.WritableNativeArray
+import com.facebook.react.bridge.WritableNativeMap
 import com.facebook.react.modules.core.PermissionAwareActivity
 import com.facebook.react.modules.core.PermissionListener
 
 class NativeMobileSDKBridge(
         reactContext: ReactApplicationContext,
         private val eventEmitter: RNEventEmitter,
-        private val meetingObservers: MeetingObservers) : ReactContextBaseJavaModule(reactContext), PermissionListener {
+        private val meetingObservers: MeetingObservers
+) : ReactContextBaseJavaModule(reactContext), PermissionListener {
 
     companion object {
         private const val WEBRTC_PERMISSION_REQUEST_CODE = 1
@@ -49,11 +55,12 @@ class NativeMobileSDKBridge(
 
     private val logger = ConsoleLogger(LogLevel.DEBUG)
 
-    private val webRtcPermissionPermission = arrayOf(
-            Manifest.permission.MODIFY_AUDIO_SETTINGS,
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.CAMERA
-    )
+    private val webRtcPermissionPermission =
+            arrayOf(
+                    Manifest.permission.MODIFY_AUDIO_SETTINGS,
+                    Manifest.permission.RECORD_AUDIO,
+                    Manifest.permission.CAMERA
+            )
 
     override fun getName(): String {
         return "NativeMobileSDKBridge"
@@ -71,31 +78,41 @@ class NativeMobileSDKBridge(
 
             try {
                 val sessionConfig = createSessionConfiguration(meetingInfo, attendeeInfo)
-                val meetingSession = sessionConfig?.let {
-                    DefaultMeetingSession(
-                            it,
-                            logger,
-                            activity.applicationContext
-                    )
-                }
+                val meetingSession =
+                        sessionConfig?.let {
+                            DefaultMeetingSession(it, logger, activity.applicationContext)
+                        }
 
                 if (meetingSession != null) {
                     NativeMobileSDKBridge.meetingSession = meetingSession
 
                     if (!hasPermissionsAlready()) {
                         val permissionAwareActivity = activity as PermissionAwareActivity
-                        permissionAwareActivity.requestPermissions(webRtcPermissionPermission, WEBRTC_PERMISSION_REQUEST_CODE, this)
+                        permissionAwareActivity.requestPermissions(
+                                webRtcPermissionPermission,
+                                WEBRTC_PERMISSION_REQUEST_CODE,
+                                this
+                        )
                         return
                     }
 
                     startAudioVideo()
                 } else {
                     logger.error(TAG, "Failed to create meeting session")
-                    eventEmitter.sendReactNativeEvent(RN_EVENT_ERROR, "Failed to create meeting session")
+                    eventEmitter.sendReactNativeEvent(
+                            RN_EVENT_ERROR,
+                            "Failed to create meeting session"
+                    )
                 }
             } catch (exception: Exception) {
-                logger.error(TAG, "Error starting the meeting session: ${exception.localizedMessage}")
-                eventEmitter.sendReactNativeEvent(RN_EVENT_ERROR, "Error starting the meeting session: ${exception.localizedMessage}")
+                logger.error(
+                        TAG,
+                        "Error starting the meeting session: ${exception.localizedMessage}"
+                )
+                eventEmitter.sendReactNativeEvent(
+                        RN_EVENT_ERROR,
+                        "Error starting the meeting session: ${exception.localizedMessage}"
+                )
                 return
             }
         }
@@ -106,7 +123,8 @@ class NativeMobileSDKBridge(
             webRtcPermissionPermission.all {
                 ContextCompat.checkSelfPermission(activity, it) == PackageManager.PERMISSION_GRANTED
             }
-        } ?: false
+        }
+                ?: false
     }
 
     private fun startAudioVideo() {
@@ -120,7 +138,10 @@ class NativeMobileSDKBridge(
         }
     }
 
-    private fun createSessionConfiguration(meetingInfo: ReadableMap, attendeeInfo: ReadableMap): MeetingSessionConfiguration? {
+    private fun createSessionConfiguration(
+            meetingInfo: ReadableMap,
+            attendeeInfo: ReadableMap
+    ): MeetingSessionConfiguration? {
         return try {
             val meetingId = meetingInfo.getString(KEY_MEETING_ID) ?: ""
             val attendeeId = attendeeInfo.getString(KEY_ATTENDEE_ID) ?: ""
@@ -139,12 +160,23 @@ class NativeMobileSDKBridge(
                 signalingUrl = it.getString(KEY_SIGNALING_URL) ?: ""
             }
 
-            MeetingSessionConfiguration(meetingId,
+            MeetingSessionConfiguration(
+                    meetingId,
                     MeetingSessionCredentials(attendeeId, externalUserId, joinToken),
-                    MeetingSessionURLs(audioFallbackUrl, audioHostUrl, turnControlUrl, signalingUrl, ::defaultUrlRewriter))
+                    MeetingSessionURLs(
+                            audioFallbackUrl,
+                            audioHostUrl,
+                            turnControlUrl,
+                            signalingUrl,
+                            ::defaultUrlRewriter
+                    )
+            )
         } catch (exception: Exception) {
             logger.error(TAG, "Error creating session configuration: ${exception.localizedMessage}")
-            eventEmitter.sendReactNativeEvent(RN_EVENT_ERROR, "Error creating session configuration: ${exception.localizedMessage}")
+            eventEmitter.sendReactNativeEvent(
+                    RN_EVENT_ERROR,
+                    "Error creating session configuration: ${exception.localizedMessage}"
+            )
             null
         }
     }
@@ -180,16 +212,17 @@ class NativeMobileSDKBridge(
 
     @ReactMethod
     fun bindVideoView(viewIdentifier: Double, tileId: Int) {
-        logger.info(TAG, "Called bindVideoView for tileId: $tileId with identifier: $viewIdentifier")
+        logger.info(
+                TAG,
+                "Called bindVideoView for tileId: $tileId with identifier: $viewIdentifier"
+        )
     }
 
     @ReactMethod
     fun unbindVideoView(tileId: Int) {
         logger.info(TAG, "Called unbindVideoView for tileId: $tileId")
 
-        meetingSession?.run {
-            audioVideo.unbindVideoView(tileId)
-        }
+        meetingSession?.run { audioVideo.unbindVideoView(tileId) }
     }
 
     @ReactMethod
@@ -197,14 +230,24 @@ class NativeMobileSDKBridge(
         meetingSession?.audioVideo?.realtimeSendDataMessage(topic, message, lifetimeMs)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>?, grantResults: IntArray?): Boolean {
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>?,
+            grantResults: IntArray?
+    ): Boolean {
         return when (requestCode) {
             WEBRTC_PERMISSION_REQUEST_CODE -> {
                 val isMissingPermission: Boolean =
-                        grantResults?.isEmpty() ?: false || grantResults?.any { PackageManager.PERMISSION_GRANTED != it } ?: false
+                        grantResults?.isEmpty()
+                                ?: false ||
+                                grantResults?.any { PackageManager.PERMISSION_GRANTED != it }
+                                        ?: false
 
                 if (isMissingPermission) {
-                    eventEmitter.sendReactNativeEvent(RN_EVENT_ERROR, "Unable to start meeting as permissions are not granted")
+                    eventEmitter.sendReactNativeEvent(
+                            RN_EVENT_ERROR,
+                            "Unable to start meeting as permissions are not granted"
+                    )
                     false
                 } else {
                     startAudioVideo()
@@ -215,14 +258,91 @@ class NativeMobileSDKBridge(
         }
     }
 
+    @ReactMethod
+    fun getAudieDevicesList(promise: Promise) {
+        try {
+            val listAudioDevices = meetingSession?.audioVideo?.listAudioDevices()
+            val map: WritableMap = WritableNativeMap()
+            val list: WritableArray = WritableNativeArray()
+
+            listAudioDevices?.forEach {
+                val device: WritableMap = WritableNativeMap()
+                device.putString("id", it.id)
+                device.putString("label", it.label)
+                device.putString("type", it.type.name)
+                list.pushMap(device)
+            }
+
+            map.putArray("devices", list)
+            promise.resolve(map)
+        } catch (e: Exception) {
+            promise.reject(
+                    "GET_AUDIO_DEVICES_ERROR",
+                    "Error fetching audio devices: ${e.localizedMessage}"
+            )
+        }
+    }
+
+    @ReactMethod
+    fun switchMicrophoneToSpeaker(promise: Promise) {
+        logger.info(TAG, "Called switchMicrophoneWithSpeaker")
+        // promise.resolve(MediaDeviceType.AUDIO_BUILTIN_SPEAKER)
+        try {
+            val listAudioDevices = meetingSession?.audioVideo?.listAudioDevices()
+
+            listAudioDevices?.let { devices ->
+                // Ensure that the list is not empty before accessing its type
+                if (devices.size >= 2) {
+                    val currentDeviceType = meetingSession?.audioVideo?.getActiveAudioDevice()?.type
+                    val newDeviceType =
+                            if (currentDeviceType == MediaDeviceType.AUDIO_BUILTIN_SPEAKER) {
+                                MediaDeviceType.AUDIO_HANDSET
+                            } else {
+                                MediaDeviceType.AUDIO_BUILTIN_SPEAKER
+                            }
+
+                    val newDevice = devices.find { it.type == newDeviceType }
+                    newDevice?.let {
+                        meetingSession?.audioVideo?.chooseAudioDevice(it)
+                        promise.resolve("Microphone to speaker switched successfully")
+                    }
+                            ?: promise.reject(
+                                    "SWITCH_MICROPHONE_SPEAKER_ERROR",
+                                    "New device is null or not found in the list"
+                            )
+                } else {
+                    promise.reject(
+                            "SWITCH_MICROPHONE_SPEAKER_ERROR",
+                            "Audio devices list is empty or insufficient"
+                    )
+                }
+            }
+                    ?: promise.reject(
+                            "SWITCH_MICROPHONE_SPEAKER_ERROR",
+                            "Audio devices list is null"
+                    )
+        } catch (e: Exception) {
+            val errorMessage = "Error switching microphone and speaker: ${e.localizedMessage}"
+            logger.error(TAG, errorMessage)
+            promise.reject("SWITCH_MICROPHONE_SPEAKER_ERROR", errorMessage)
+        }
+    }
+
+    @ReactMethod
+    fun switchCamera(promise: Promise) {
+        logger.info(TAG, "Called switchCamera")
+
+        try {
+            meetingSession?.audioVideo?.switchCamera()
+            promise.resolve("Camera switched successfully")
+        } catch (e: Exception) {
+            logger.error(TAG, "Error switching camera: ${e.localizedMessage}")
+            promise.reject("SWITCH_CAMERA_ERROR", "Error switching camera: ${e.localizedMessage}")
+        }
+    }
+
     // Required for rn built in EventEmitter Calls.
-    @ReactMethod
-    fun addListener(eventName: String) {
+    @ReactMethod fun addListener(eventName: String) {}
 
-    }
-
-    @ReactMethod
-    fun removeListeners(count: Int) {
-
-    }
+    @ReactMethod fun removeListeners(count: Int) {}
 }
