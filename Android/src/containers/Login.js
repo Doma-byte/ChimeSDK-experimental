ddc3bbd1b1af3d79461c43cf3d2d3c7904721d32
Dikshit <singhdikshit69@gmail.com>
2024-01-24 15:08:32 +0530

    fix(Login.js): fix the usage of meetingName and userName variables by converting them to state properties to ensure proper reactivity and data binding feat(Login.js): add functionality to generate a unique meeting name using UUIDGenerator library and display it in the input field feat(Login.js): add a button to generate a new UUID and update the meetingName state property accordingly feat(Login.js): update the onChangeText event handler for the userName input field to update the userName state property

--- Android/src/containers/Login.js
+++ Android/src/containers/Login.js
@@ -1,27 +1,54 @@
 import React from 'react';
 import { View, Text, TextInput, Button, Alert } from 'react-native';
+import UUIDGenerator from "react-native-uuid-generator";
 import styles from '../Style';
 
 export class Login extends React.Component {
   constructor() {
     super();
-    this.meetingName = "";
-    this.userName = "";
+    this.state = {
+      meetingName: "",
+      userName: "",
+    };
+  }
+
+  generateUniqueMeetingName = async () => {
+    try {
+      const uuid = await UUIDGenerator.getRandomUUID();
+      this.setState({ meetingName: uuid });
+      console.log("MEETING : ", this.state.meetingName);
+    } catch (error) {
+      console.error("Error generating random values:", error);
+    }
+  };
+
+  handleGenerateUUID = () => {
+    this.generateUniqueMeetingName();
   }
 
   startMeeting = () => {
-    if (!this.meetingName || !this.userName) {
-      Alert.alert("Meeting name and user name can not be empty");
+    if (!this.state.userName) {
+      Alert.alert("User name cannot be empty");
     } else {
-      this.props.onSubmit(this.meetingName, this.userName);
+      this.props.onSubmit(this.state.meetingName, this.state.userName);
     }
   }
 
   renderForm() {
     return (
       <React.Fragment>
-        <TextInput style={styles.inputBox} placeholder="Meeting ID" onChangeText={(val) => this.meetingName = val.trim()} />
-        <TextInput style={styles.inputBox} placeholder="Your Name" onChangeText={(val) => this.userName = val.trim()} />
+        <TextInput
+          style={styles.inputBox}
+          value={this.state.meetingName}
+          editable={false}
+          placeholder="Auto-generated UUID"
+        />
+        <Button title='Generate UUID' onPress={this.handleGenerateUUID}></Button>
+       <TextInput
+          style={styles.inputBox}
+          placeholder="Your Name"
+          onChangeText={(val) => this.setState({ userName: val.trim() })}
+        />
         <Button title="Start" onPress={this.startMeeting} />
       </React.Fragment>
     );
