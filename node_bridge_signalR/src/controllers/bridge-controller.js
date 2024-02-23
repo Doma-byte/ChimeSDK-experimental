1e58d1477df0f07b369d19492d0c4702999a6b4e
Dikshit <singhdikshit69@gmail.com>
2024-02-23 18:00:33 +0530

    fix(bridge-controller.js): fix variable naming convention for user_id and meetingId to improve readability feat(bridge-controller.js): add sendMessage function to handle sending messages fix(bridge-repository.js): add empty functions for incomingCall, outgoingCall, and receiveMessage to be implemented later feat(bridge-repository.js): add sendMessage function to handle sending messages

--- node_bridge_signalR/src/controllers/bridge-controller.js
+++ node_bridge_signalR/src/controllers/bridge-controller.js
@@ -3,7 +3,7 @@ const { BridgeService } = require("../services/index");
 const bridgeService = new BridgeService();
 const bridge = async (req, res) => {
   try {
-    const {user_id, meetingId} = req.body;
+    const { user_id, meetingId } = req.body;
     const response = await bridgeService.signalBridge(user_id, meetingId);
     return res.status(200).json({
       message: "Successfully send the data",
@@ -12,7 +12,6 @@ const bridge = async (req, res) => {
       success: true,
     });
   } catch (err) {
-    console.log(err);
     return res.status(500).json({
       data: {},
       success: false,
@@ -22,6 +21,38 @@ const bridge = async (req, res) => {
   }
 };
 
+const sendMessage = async (req, res) => {
+  try {
+    const formData = req.body;
+    console.log("Request body is : ",req.body);
+    const {
+      MemberToken,
+      MessageId,
+      Message,
+      ReceiverID,
+      IsCallMsg,
+      EventType,
+      AttachedMedia,
+    } = formData;  
+    console.log(formData);
+    const response = await bridgeService.sendMessageService(formData);
+    return res.status(200).json({
+      message: "Successfully send the data",
+      error: {},
+      data: response,
+      success: true,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      data: {},
+      success: false,
+      error: err,
+      message: "Not able to send message",
+    });
+  }
+};
+
 module.exports = {
   bridge,
+  sendMessage,
 };
--- node_bridge_signalR/src/repository/bridge-repository.js
+++ node_bridge_signalR/src/repository/bridge-repository.js
@@ -16,6 +16,33 @@ class BrideRepository {
       throw { err };
     }
   }
+
+  async incomingCall(sender_id, receiver_id){
+      try{
+
+      }catch(err){
+        console.log("Something went wrong in the repository layer");
+        throw { err };
+      }
+  }
+  
+  async outgoingCall(){
+
+  }
+
+  async sendMessage(msgDetails){
+      try{
+        return msgDetails;
+      }catch(err){
+        console.log("Something went wrong in the repository layer");
+        throw { err };
+      }
+  }
+  
+  async receiveMessage(){
+
+  }
+  
 }
 
 module.exports = BrideRepository;
